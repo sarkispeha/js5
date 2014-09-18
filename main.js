76a4ccda5345ea805b510e5a53877c695aec470f
Sarkis Peha <sarkispeha@gmail.com>
2014-09-18 11:01:12 -0600

    event handlers for textbox hide and show working

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 $(document).on('ready', function() {
   
 	$('.textbox').click(function() {
-		$('.textbox').hide();
-		$('.textshow').show();
+		$('.textbox').hide(500);
+		$('.textshow').show(500);
 	})
 
+	$('textarea').blur(function() {
+		$('.textshow').hide(500);
+		$('.textbox').show(500);
 
+	})
 
 
 
